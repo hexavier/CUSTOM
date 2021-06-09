edcd7a4943e38413fac45f17b30a30770b992971
Xavier Hernandez <xa.he.al@gmail.com>
2021-06-09 18:08:25 +0200

    Add logo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TisOpt",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['LICENSE', '*.md','data/*.csv']},
